428cc7f69aeb020dade14b15ebc2d2b82c4e4a7f
Wang, Huayu <huayu.wang20@imperial.ac.uk>
2023-02-07 01:56:48 +0000

    implement functions draw_map, generate_map, and write_output

--- src/helper.py
+++ src/helper.py
@@ -11,6 +11,7 @@ def read_input(data_path):
             (rows, cols, reefs, start, end) ((int, int, [(int, int)], (int, int), (int, int))): 
                 number of rows and columns of the map, list of reef coordinates, start point coordinate, and end point coordinate
     """
+    #TODO: handle cases when input contains repeated coordinates
     rows = 0
     cols = 0
     reefs = []
@@ -26,7 +27,8 @@ def read_input(data_path):
     filtered_coordinate_list = list(filter(valid, coordinate_list))
     
     if(len(filtered_coordinate_list) < 2):
-        print("Too few input coordinates")
+    # Error: must have at least 2 coordinates in input .txt file
+        print("Too few input coordinates!")
         write_output(data_path, "error")
         exit()
         
@@ -37,11 +39,27 @@ def read_input(data_path):
     
     for reef_coordinate_text in filtered_coordinate_list:
         (x, y) = convert(reef_coordinate_text)
-        if x > cols:
-            cols = x
-        if y > rows:
-            rows = y
+        if x+1 > cols:
+            cols = x+1
+        if y+1 > rows:
+            rows = y+1
+        # Could handle errors of start/end point is a reef here, but considering performance, decided not
+        # This case will be handled by function find_shortest_path, which returns an empty path as a result
+        # So this case is a subset of no-path errors
         reefs.append((x, y))
+    
+    (x_start, y_start) = start
+    (x_end, y_end) = end
+    if(x_start >= cols or y_start >= rows):
+    # Error: start point's coordinate must be inside the map
+        print("Start point out of map!")
+        write_output(data_path, "error")
+        exit()
+    if(x_end >= cols or y_end >= rows):
+    # Error: end point's coordinate must be inside the map
+        print("End point out of map!")
+        write_output(data_path, "error")
+        exit()
         
     return (rows, cols, reefs, start, end)
 
@@ -85,6 +103,9 @@ def write_output(data_path, map):
         Return:
             success (bool): whether wrting is successful
     """
+    file = open(data_path + ".answer", "w")
+    file.write(map)
+    file.close()
     return True
 
 
@@ -101,13 +122,20 @@ def draw_map(rows, cols, reefs, start, end):
         Return:
             binary_map ([[int]]): a binary 2d list representing the map, where 0 represents reef and 1 represents sea
     """
-    return []
+    #TODO: remove start and end parameters
+    binary_map = [[1 for i in range(cols)] for j in range(rows)]
+    for reef in reefs:
+        (x, y) = reef
+        binary_map[y][x] = 0
+    return binary_map
 
 
-def generate_map(binary_map, start, end, path):
+def generate_map(rows, cols, binary_map, start, end, path):
     """
         Convert the binary map into its final text versionm where sea is '.', reef is 'x', start is 'S', end is 'E', and path are 'O'
         Args:
+            rows (int): number of rows in the map
+            cols (int): number of columns in the map
             binary_map ([[int]]): a binary 2d list representing the map, where 0 represents reef and 1 represents sea
             start ((int, int)): start point coordinate
             end ((int, int)): end point coordinate
@@ -115,18 +143,39 @@ def generate_map(binary_map, start, end, path):
         Return:
             map (string): the final map in text version, where sea is '.', reef is 'x', start is 'S', end is 'E', and path are 'O'
     """
-    return ""
+    map = ""
+    char_map = [['.' for i in range(cols)] for j in range(rows)]
+    
+    for i in range(rows):
+        for j in range(cols):
+            if not binary_map[i][j]:
+                char_map[i][j] = 'x'
+    
+    for (x, y) in path:
+        char_map[y][x] = 'O'
+    
+    (x_start, y_start) = start
+    (x_end, y_end) = end
+    char_map[y_start][x_start] = 'S'
+    char_map[y_end][x_end] = 'E'
+    
+    for i in range(rows):
+        for j in range(cols):
+            map += char_map[i][j]
+        map += '\n'
+    map = map.strip()
+    return map
 
 
-def find_shortest_path(binary_map, start_point, end_point):
+def find_shortest_path(binary_map, start_point, end_point, data_path):
     """
         Find shortest path on the binary_map based on start_point and end_point
         If no path or end_point not reachable, return []
-        If start_point or end_point out of map, return []
         Args:
             binary_map ([[int]]): 2d list containing 0s and 1s, where 0 represents reef, and 1 represents sea
             start_index ((int, int)): starting point coordinates
             end_index ((int, int)): ending point coordinates
+            data_path (string): absolute path of the input .txt file (being used here when handling error cases)
         Return:
             path ([(int, int)]): list of coordinates of all points in the path
     """
@@ -135,8 +184,13 @@ def find_shortest_path(binary_map, start_point, end_point):
         start = grid.node(start_point[0], start_point[1])
         end = grid.node(end_point[0], end_point[1])
     except IndexError:
-        #TODO: distinguish between "start/end out of map error" from "no path error"
-        return []
+    # Error: this error should never occur because of previous condition checking in function read_input
+        return[]
     finder = AStarFinder()
     path, runs = finder.find_path(start, end, grid)
+    if path == []:
+    # Error: No path (either all roads are blocked, or start/end point is at a reef)
+        print("No valid path!")
+        write_output(data_path, "error")
+        exit()
     return path
--- src/main.py
+++ src/main.py
@@ -2,12 +2,17 @@ import helper
 import sys
 
 example_path = r"C:\Users\Charles\Desktop\path-finding\input"
-example_path += "\path-malformed-rubbish.txt"
+example_path += "\path-error.txt"
 
 #TODO: handle command line arguements
+data_path = example_path
 # data_path = sys.argv[1]
-(rows, cols, reefs, start, end) = helper.read_input(example_path)
-# binary_map = helper.draw_map(rows, cols, reefs, start, end)
-# path = helper.find_shortest_path(binary_map, start, end)
-# map = helper.generate_map(binary_map, start, end, path)
-# helper.write_output(data_path, map)
+(rows, cols, reefs, start, end) = helper.read_input(data_path)
+binary_map = helper.draw_map(rows, cols, reefs, start, end)
+path = helper.find_shortest_path(binary_map, start, end, data_path)
+map = helper.generate_map(rows, cols, binary_map, start, end, path)
+helper.write_output(data_path, map)
+
+# print(rows, cols, reefs, start, end)
+# print(path)
+# print(map)
